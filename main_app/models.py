08022597ad05ccfc49d255ff4a7fec2ce350e23a
dhagandev <dhagan@utexas.edu>
2019-06-23 16:56:08 -0500

    First draft of models Profile and Question

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	q_correct = models.IntegerField()
+	q_answered = models.IntegerField()
+
+class Question(models.Model):
+	category = models.CharField(max_length=200)
+	difficulty = models.CharField(max_length=50)
+	q_text = models.TextField()
+	correct_ans = models.TextField()
+	incorrect_ans = ArrayField(models.TextField())
